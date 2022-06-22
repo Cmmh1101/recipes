bf45919dd51f6ba110f96a9d42fb1df0e953f692
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-06-21 22:29:19 -0500

    initial project setup

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-export default function Home() {
+const contact = () => {
   return (
     <Layout>
-      <h1>home </h1>
+      <h1>contact page</h1>
     </Layout>
   )
 }
+
+export default contact
